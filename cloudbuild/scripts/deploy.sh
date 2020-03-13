dacd31ad20e4a0bade1efb7135d33d21328979b7
Matt Beken <=>
2020-03-13 11:45:58 -0600

    Add some build comments

--- cloudbuild/scripts/deploy.sh
+++ cloudbuild/scripts/deploy.sh
@@ -1,3 +1,3 @@
-#!/usr/bin/env bash
-
-twine upload dist/* -u $_PYPI_USERNAME -p $PYPI_PASSWORD
+code_version=$(python setup.py --version) && \
+pip_version=$(yolk -V pyslo | cut -d" " -f2) && \
+([ "$code_version" != "$pip_version" ] && twine upload dist/* -u $_PYPI_USERNAME -p $PYPI_PASSWORD || echo "No new version")
