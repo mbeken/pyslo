5d07c5f1f0f222261c620849186bc60804aec2db
Matt Beken <=>
2020-03-16 16:47:19 -0600

    specify master_doc = index

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -24,6 +24,7 @@ author = 'Matt Beken'
 # The full version, including alpha/beta/rc tags
 release = '0.0.2'
 
+master_doc = 'index'
 
 # -- General configuration ---------------------------------------------------
 
